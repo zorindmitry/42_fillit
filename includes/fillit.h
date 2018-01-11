a20c212084da63f0f2657318e40745a2ca04353c
Dmytro ZORIN <dzorin@e1r7p10.unit.ua>
2018-01-11 15:31:53 +0200

    add start files

---
+++ includes/fillit.h
@@ -0,0 +1,8 @@
+#ifndef FILLIT_H
+# define FILLIT_T
+# include <string.h>
+# include "includes\libft.h"
+//
+//
+//
+#endif
---
+++ src/main.c
@@ -0,0 +1,13 @@
+///PRE///
+
+int		main(int argc, char **argv)
+{
+	//init
+
+	if (argc != 2)	//what if folder?//
+	{
+		ft_putstr("Use only fillit input file"); //?
+		return (1);
+	}
+	return (0);
+}
