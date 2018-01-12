3f8b07f66a22627f6d6178d58a4e01bea81e254b
Dmytro ZORIN <dzorin@e1r7p10.unit.ua>
2018-01-12 14:58:12 +0200

    add types in .h file

--- includes/fillit.h
+++ includes/fillit.h
@@ -5,5 +5,27 @@
 # include "includes\libft.h"
 
 
+
+
+typedef struct	s_map
+{
+	int			size;
+	char		**array;
+}				t_map;
+
+typedef struct s_tetriminos
+{
+	char **tetr;
+	int x;
+	int y;
+	char value; ///?
+}				t_tetriminos;
+
 t_list	*read_in_list(int fd);
+
+
+t_map			*solve(t_list *list);
+void			print_map(t_map *map);
+void			free_map(t_map *map);
+void			free_list(t_list *list);
 #endif
--- src/read.c
+++ src/read.c
@@ -6,13 +6,20 @@
 
 t_list	*read_in_list(int fd)
 {
-	char	name;
-	char	*buff;
-	t_list	*list;
-	int		quantity_of_tetrimins
+	char			name;
+	char			*buff;
+	t_list			*list;
+	tetriminos_one	*t_tetriminos;
+	int				count;
 
 	buff = ft_strnew(21);
 	name = 'A';
 
-	while (quantity_of_tetrimins = read(fd, buf, 21))
+	while (count = read(fd, buff, 21))
+	{
+		if (!(tetriminos_one = get_one(buff, cur))) // ||check
+		{
+			//del free 
+		}
+	}
 }
